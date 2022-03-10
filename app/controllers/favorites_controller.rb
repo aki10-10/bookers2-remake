1c134450dc26b316044d1aa6ef5204ff241b2a79
EC2 Default User <ec2-user@ip-172-31-5-110.ec2.internal>
2022-03-10 01:25:02 +0000

    ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
   
 
 def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer　非同期化のため　削除
 end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer 非同期化のため削除
   end
 end
